3ea377832002a9c5ee35876f2729451299a5bdd1
pierrelh <lhopital.pierre@gmail.com>
2018-11-25 19:10:52 +0100

    Dynamic chat with Ajax add

--- functions/getDB.php
+++ functions/getDB.php
@@ -1,13 +1,13 @@
 <?php
 
-function connect(){
-  try{
-    $bdd = new PDO('mysql:host=127.0.0.1;dbname=chat', 'root', '');
-    return $bdd;
+  function connect(){
+    try{
+      $bdd = new PDO('mysql:host=127.0.0.1;dbname=chat', 'root', '');
+      return $bdd;
+    }
+    catch(Exception $e){
+      die('Erreur : '.$e->getMessage());
+    }
   }
-  catch(Exception $e){
-    die('Erreur : '.$e->getMessage());
-  }
-}
 
 ?>
